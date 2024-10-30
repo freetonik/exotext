768df2cb680002d09a456a30f3676b08f46e4c0e
Rakhim Davletkaliyev <hello@rakhim.org>
2024-10-30 23:44:33 +0200

    feat: add new blog post button for author

--- src/blogs.ts
+++ src/blogs.ts
@@ -29,13 +29,9 @@ export const handleBlog = async (c: Context) => {
     const blog = batch[0].results[0];
     const posts = batch[1].results;
 
-    const addButton =
-        userLoggedIn && userId === blog.user_id
-            ? `<a href="/${subdomain}/new"><svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg"><path d='M3 9.4c0-2.24 0-3.36.436-4.216a4 4 0 0 1 1.748-1.748C6.04 3 7.16 3 9.4 3h5.2c2.24 0 3.36 0 4.216.436a4 4 0 0 1 1.748 1.748C21 6.04 21 7.16 21 9.4v5.2c0 2.24 0 3.36-.436 4.216a4 4 0 0 1-1.748 1.748C17.96 21 16.84 21 14.6 21H9.4c-2.24 0-3.36 0-4.216-.436a4 4 0 0 1-1.748-1.748C3 17.96 3 16.84 3 14.6zM15.5 12H12m0 0H8.5m3.5 0V8.5m0 3.5v3.5'/></svg></a>`
-            : '';
     let list = `
         <header class="blog-header">
-            <h1>${blog.title}${addButton}</h1>
+            <h1>${blog.title}</h1>
             
             <p class="blog-description">Observations on technology, literature, and the spaces between. Written sporadically, with care.</p>
         </header>
